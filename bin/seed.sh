7d4d14e4b8740fd3a86dac5843680341cc49e8a8
rio84 <rio84@qq.com>
2016-09-25 15:39:08 +0800

    seed

---
+++ bin/seed.sh
@@ -0,0 +1,27 @@
+DIR=./tmp-autopub-239884
+INSTALL_PATH=~/localhost
+TAR_NAME=autopub.tar.gz
+if [ x$1 != x ]
+then
+    INSTALL_PATH=$1
+fi
+mkdir $DIR
+echo "install dir is: "$INSTALL_PATH
+curl -o $DIR/$TAR_NAME https://codeload.github.com/rio84/autopub/legacy.tar.gz/latest
+
+tar -xvf $DIR/$TAR_NAME -C $DIR
+
+rm -f $DIR/$TAR_NAME
+
+mv $DIR/* $INSTALL_PATH/autopub
+rm -fr $DIR
+cd $INSTALL_PATH/autopub
+echo "CNPM installing..."
+cnpm install
+echo "starting app.js. log to './autopub.log'"
+node app.js > autopub.log &
+
+echo "DONE"
+
+
+
